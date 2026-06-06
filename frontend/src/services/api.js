40843337f5079ea0bb0a0a9c3ffcf3ddf8b5f5cc
Shivangi Tripathi <shivangitripathi2225@gmail.com>
2026-06-07 01:03:48 +0530

    frontend deployment config

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,5 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL ||
-    "http://localhost:8000"
+export default axios.create({
+  baseURL: import.meta.env.VITE_API_URL
 });
-
-export default api;
